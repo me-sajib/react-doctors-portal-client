f756e743791e1cc617bd1b961d1e851de5a14e72
sajib <me.mrsajib@gmail.com>
2022-05-10 13:49:18 +0600

    primary button component add and text change

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,20 +1,19 @@
 import React from "react";
 import bannerImg from "../../assets/images/chair.png";
+import PrimaryBtn from "../Shared/PrimaryBtn";
 const Banner = () => {
   return (
-    <div class="hero min-h-screen">
-      <div class="hero-content flex-col lg:flex-row-reverse mx-14">
+    <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
         <img src={bannerImg} className="max-w-sm rounded-lg shadow-2xl" />
         <div>
-          <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
-          <p class="py-6">
+          <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+          <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button class="btn btn-primary bg-gradient-to-r from-secondary to-primary">
-            Get Started
-          </button>
+          <PrimaryBtn />
         </div>
       </div>
     </div>
