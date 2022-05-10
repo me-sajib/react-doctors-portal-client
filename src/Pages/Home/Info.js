635b0bc859d5f182f4acff060b4eb6c6bf40009a
sajib <me.mrsajib@gmail.com>
2022-05-10 12:40:10 +0600

    set to image , color and title

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,12 +1,22 @@
 import React from "react";
 import InfoCard from "./InfoCard";
 import clock from "../../assets/icons/clock.svg";
+import marker from "../../assets/icons/marker.svg";
+import phone from "../../assets/icons/phone.svg";
 const Info = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
-      <InfoCard img={clock} />
-      <InfoCard img={clock} />
-      <InfoCard img={clock} />
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mx-14">
+      <InfoCard
+        title="Opening Hours"
+        img={clock}
+        bgColor="bg-gradient-to-r from-secondary to-primary"
+      />
+      <InfoCard title="Visit our location" img={marker} bgColor="bg-accent" />
+      <InfoCard
+        title="Contact us now"
+        img={phone}
+        bgColor="bg-gradient-to-r from-secondary to-primary"
+      />
     </div>
   );
 };
