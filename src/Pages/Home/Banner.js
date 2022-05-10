56f4dc5af5c0154e10a654f8ed839b0dcfb01ec6
sajib <me.mrsajib@gmail.com>
2022-05-10 12:20:04 +0600

    change banner text and button color change

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -3,16 +3,18 @@ import bannerImg from "../../assets/images/chair.png";
 const Banner = () => {
   return (
     <div class="hero min-h-screen">
-      <div class="hero-content flex-col lg:flex-row-reverse">
+      <div class="hero-content flex-col lg:flex-row-reverse mx-14">
         <img src={bannerImg} className="max-w-sm rounded-lg shadow-2xl" />
         <div>
-          <h1 class="text-5xl font-bold">Box Office News!</h1>
+          <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
           <p class="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button class="btn btn-primary">Get Started</button>
+          <button class="btn btn-primary bg-gradient-to-r from-secondary to-primary">
+            Get Started
+          </button>
         </div>
       </div>
     </div>
