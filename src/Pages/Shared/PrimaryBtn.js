2aee00c1b774d23b967701bc7328e717cfa0db07
sajib <me.mrsajib@gmail.com>
2022-05-10 13:47:47 +0600

    primary button page add

---
+++ src/Pages/Shared/PrimaryBtn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrimaryBtn = () => {
+  return (
+    <button className="btn btn-primary bg-gradient-to-r from-secondary to-primary">
+      Get Started
+    </button>
+  );
+};
+
+export default PrimaryBtn;
