780515bbb9bea038fcd01ed0529c1273bdaaef80
sajib <me.mrsajib@gmail.com>
2022-05-10 13:50:56 +0600

    info card grid change

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -5,7 +5,7 @@ import marker from "../../assets/icons/marker.svg";
 import phone from "../../assets/icons/phone.svg";
 const Info = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mx-14">
+    <div className="my-16 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
       <InfoCard
         title="Opening Hours"
         img={clock}
