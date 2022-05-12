70362660d10e98e59d3861f8f64db2c192f24c0b
sajib <me.mrsajib@gmail.com>
2022-05-12 14:09:31 +0600

    loading page add

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div class=" flex justify-center items-center">
+    <div class=" flex justify-center items-center h-screen">
       <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
     </div>
   );
